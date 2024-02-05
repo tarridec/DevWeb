c98ac9d9b84169b62d8488a86a528010131a2995
tarridec <maelys.tarridec@gmail.com>
2024-02-05 11:27:12 +0100

    Exo 2 fait

--- 02_ternaire.js
+++ 02_ternaire.js
@@ -6,6 +6,11 @@
  *    - utiliser l'opérateur ternaire (if interdit)
  */
 function ternaire(a , b) {
+    a = 2
+    b = 5
+
+    return a == b ? "a et b sont égaux" :
+           a > b ? "a est plus grand que b" : "b est plus grand que a"
 }
 
 console.log(ternaire(1, 2));
